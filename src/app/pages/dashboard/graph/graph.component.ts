a3360a82ea13d628039fd4dba6b484b10991a855
Ugo El Koubi <eu201468@etu.unice.fr>
2017-01-20 11:40:23 +0100

    connections between edges

--- src/app/pages/dashboard/graph/graph.component.ts
+++ src/app/pages/dashboard/graph/graph.component.ts
@@ -5,6 +5,7 @@ import {GraphService} from './graph.service';
 import './graph.loader.ts';
 import {cytoscape} from "./graph.loader";
 import {contextMenus} from "./graph.loader";
+import {edgehandles} from "./graph.loader";
 import {jquery} from "./graph.loader";
 @Component({
   selector: 'graph',
@@ -149,8 +150,59 @@ export class Graph {
       ]
     };
     contextMenus(cytoscape, jquery); // register extension
-
     this.cy.contextMenus(options);
+
+    // the default values of each option are outlined below:
+    var defaults = {
+      preview: true, // whether to show added edges preview before releasing selection
+      stackOrder: 4, // Controls stack order of edgehandles canvas element by setting it's z-index
+      handleSize: 10, // the size of the edge handle put on nodes
+      handleColor: '#3e45ff', // the colour of the handle and the line drawn from it
+      handleLineType: 'ghost', // can be 'ghost' for real edge, 'straight' for a straight line, or 'draw' for a draw-as-you-go line
+      handleLineWidth: 1, // width of handle line in pixels
+      handleIcon: false, // Pass an Image-object to use as icon on handle. Icons are resized according to zoom and centered in handle.
+      handleOutlineColor: '#000000', // the colour of the handle outline
+      handleOutlineWidth: 0, // the width of the handle outline in pixels
+      handleNodes: 'node', // selector/filter function for whether edges can be made from a given node
+      handlePosition: 'middle top', // sets the position of the handle in the format of "X-AXIS Y-AXIS" such as "left top", "middle top"
+      hoverDelay: 150, // time spend over a target node before it is considered a target selection
+      cxt: false, // whether cxt events trigger edgehandles (useful on touch)
+      enabled: true, // whether to start the extension in the enabled state
+      toggleOffOnLeave: false, // whether an edge is cancelled by leaving a node (true), or whether you need to go over again to cancel (false; allows multiple edges in one pass)
+      edgeType: function( sourceNode, targetNode ) {
+        // can return 'flat' for flat edges between nodes or 'node' for intermediate node between them
+        // returning null/undefined means an edge can't be added between the two nodes
+        return 'flat';
+      },
+      loopAllowed: function( node ) {
+        // for the specified node, return whether edges from itself to itself are allowed
+        return false;
+      },
+      nodeLoopOffset: -50, // offset for edgeType: 'node' loops
+      nodeParams: function( sourceNode, targetNode ) {
+        // for edges between the specified source and target
+        // return element object to be passed to cy.add() for intermediary node
+        return {};
+      },
+      edgeParams: function( sourceNode, targetNode, i ) {
+        // for edges between the specified source and target
+        // return element object to be passed to cy.add() for edge
+        // NB: i indicates edge index in case of edgeType: 'node'
+        return {};
+      },
+      start: function( sourceNode ) {
+        // fired when edgehandles interaction starts (drag on handle)
+      },
+      complete: function( sourceNode, targetNodes, addedEntities ) {
+        // fired when edgehandles is done and entities are added
+      },
+      stop: function( sourceNode ) {
+        // fired when edgehandles interaction is stopped (either complete with added edges or incomplete)
+      }
+    };
+
+    edgehandles( cytoscape ); // register extension
+    this.cy.edgehandles( defaults );
   }
 
 
--- src/app/pages/dashboard/graph/graph.loader.ts
+++ src/app/pages/dashboard/graph/graph.loader.ts
@@ -1,3 +1,4 @@
 export var cytoscape = require('cytoscape');
 export var contextMenus = require('cytoscape-context-menus');
 export var jquery = require('jquery');
+export var edgehandles = require('cytoscape-edgehandles');
